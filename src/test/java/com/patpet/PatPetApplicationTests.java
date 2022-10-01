a8abb73463d0449f0363d73682b78eab2cdd3328
DESKTOP-1E01CNG\Ryu <starjisu519@naver.com>
2022-10-01 21:25:50 +0900

    [류지수] - 최초 서버 설정 및 테스트

---
+++ src/test/java/com/patpet/PatPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
