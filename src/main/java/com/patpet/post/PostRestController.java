11e146696b49ef522342c7440576d58d0c677d33
DESKTOP-1E01CNG\Ryu <starjisu519@naver.com>
2022-10-10 16:32:07 +0900

    [류지수] - 업로드 에러 확인 중 ( 400 에러 ) - nunbub/PatPet#5

--- src/main/java/com/patpet/post/PostRestController.java
+++ src/main/java/com/patpet/post/PostRestController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,6 +20,7 @@ public class PostRestController {
 	@Autowired
 	private PostBO postBO;
 
+	@PostMapping("/post/create")
 	public Map<String, String> create(
 			@RequestParam("title") String title
 			, @RequestParam("name") String name
